f9f8cc477c4895d33a656eb78cf1294aa8405e22
Fiskr <bjf092@gmail.com>
2013-03-26 12:08:13 -0400

    Oops- missed one

---
+++ controller/transactionForm.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Disconnect the Dots</title>
+<link rel="stylesheet" type="text/css" href="../geldbuch.css"/>
+<link rel="shortcut icon" href="../images/purplemushroom.ico"/>
+<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.0.js"></script>
+<script type = "text/javascript" src = "../geldbuch.js"></script>
+
+</head>
+
+<body>
+<div id='sourceForm'>
+<form action="insertTransaction.php" method="post">
+Date: <input type="text" id="inAccountName" value=""></input>
+Account: <input type="text" id="inInstitutionName" value=""></input>
+Description: <input type="text" id="inAccountName" value=""></input>
+Amount: <input type="text" id="inInstitutionName" value=""></input>
+
+<input type="submit" value="Connect"/>
+</form>
+</div>
+
+</body>
+</html>
